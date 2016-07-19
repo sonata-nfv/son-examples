943dcf8236c12dda58b7726e2db7de9ecc27d77b
Manuel Peuster <manuel@peuster.de>
2016-07-19 13:21:41 +0200

    jenkins script

--- service-projects/pack.sh
+++ service-projects/pack.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
 set -e
 
+# ensure that the latest version of son-cli is installed
+#sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys D0DF34A30A4FE3F8
+#echo "deb http://registry.sonata-nfv.eu:8080 ubuntu-trusty main" | sudo tee -a /etc/apt/sources.list
+sudo apt-get update
+sudo apt-get install sonata-cli
+
+# it might also be possible to run son-cli inside a dedicated Docker container. But the schema files seem to be missing.
+#docker pull registry.sonata-nfv.eu:5000/son-cli
+#docker run --rm -v $(pwd):/packages -w /packages registry.sonata-nfv.eu:5000/son-cli /bin/bash -c "son-workspace --init; son-package --project sonata-empty-service-emu -n sonata-empty-service"
+
 son-workspace --init --workspace test_ws
 
 son-package --workspace test_ws --project sonata-empty-service-emu -n sonata-empty-service
 son-package --workspace test_ws --project sonata-snort-service-emu -n sonata-snort-service
+
+rm -rf test_ws
