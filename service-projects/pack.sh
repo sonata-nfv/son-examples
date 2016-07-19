a805a813a55b56d0d00a96ceb888a4781d203521
Manuel Peuster <manuel@peuster.de>
2016-07-19 13:31:33 +0200

    jenkins

--- service-projects/pack.sh
+++ service-projects/pack.sh
@@ -5,7 +5,7 @@ set -e
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys D0DF34A30A4FE3F8
 echo "deb http://registry.sonata-nfv.eu:8080 ubuntu-trusty main" | sudo tee -a /etc/apt/sources.list
 sudo apt-get update
-sudo apt-get install sonata-cli
+sudo apt-get install -y sonata-cli
 
 # it might also be possible to run son-cli inside a dedicated Docker container. But the schema files seem to be missing.
 #docker pull registry.sonata-nfv.eu:5000/son-cli
