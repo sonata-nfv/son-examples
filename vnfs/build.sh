708646898609d7f1b2a4752f5e564b795f9daa91
Manuel Peuster <manuel@peuster.de>
2016-07-19 10:34:03 +0200

    renaming to indicate docker/emu vnfs/services

--- vnfs/build.sh
+++ vnfs/build.sh
@@ -9,10 +9,10 @@ set -e
 target_repo=${1-registry.sonata-nfv.eu:5000}
 
 # empty vnf
-docker build -t $target_repo/sonata-empty-vnf -f sonata-empty-vnf/Dockerfile sonata-empty-vnf
+docker build -t $target_repo/sonata-empty-vnf -f sonata-empty-vnf-docker/Dockerfile sonata-empty-vnf-docker
 
 # iperf3 vnf
-docker build -t $target_repo/sonata-iperf3-vnf -f sonata-iperf3-vnf/Dockerfile sonata-iperf3-vnf
+docker build -t $target_repo/sonata-iperf3-vnf -f sonata-iperf3-vnf-docker/Dockerfile sonata-iperf3-vnf-docker
 
 # snort IDS vnf
-docker build -t $target_repo/sonata-snort-ids-vnf -f sonata-snort-ids-vnf/Dockerfile sonata-snort-ids-vnf
+docker build -t $target_repo/sonata-snort-ids-vnf -f sonata-snort-ids-vnf-docker/Dockerfile sonata-snort-ids-vnf-docker
