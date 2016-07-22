0b19e54789db29532a8f412a7a165d722ceb4b62
Manuel Peuster <manuel@peuster.de>
2016-07-22 09:35:53 +0200

    cleanup

--- service-projects/pack.sh
+++ service-projects/pack.sh
@@ -2,20 +2,9 @@
 
 # CI entry point
 # automatic packing of service projects to validate them
-# (uses son-cli tools installed from *.deb package)
+# (uses son-cli tools installed directly from git)
 
 set -e
-
-# ensure that the latest version of son-cli is installed
-#sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys D0DF34A30A4FE3F8
-#echo "deb http://registry.sonata-nfv.eu:8080 ubuntu-trusty main" | sudo tee -a /etc/apt/sources.list
-#sudo apt-get update
-#sudo apt-get install -y sonata-cli
-#sudo apt-get remove -y sonata-cli
-
-# it might also be possible to run son-cli inside a dedicated Docker container. But the schema files seem to be missing.
-#docker pull registry.sonata-nfv.eu:5000/son-cli
-#docker run --rm -v $(pwd):/packages -w /packages registry.sonata-nfv.eu:5000/son-cli /bin/bash -c "son-workspace --init; son-package --project sonata-empty-service-emu -n sonata-empty-service"
 
 git clone https://github.com/sonata-nfv/son-cli.git
 cd son-cli
