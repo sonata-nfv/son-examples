b3ace0961106eefe1fec5c9b4f5c9655e4f9f6be
Manuel Peuster <manuel@peuster.de>
2016-08-17 11:28:44 +0200

    script now installs kernel module persistantly

--- vnfs/sonata-vtc-vnf-docker/prepare_host.sh
+++ vnfs/sonata-vtc-vnf-docker/prepare_host.sh
@@ -2,16 +2,25 @@
 #
 # PF_RING requires some additional kernel modules to be installed on the HOST machine to be able to execute the vTC docker container correctly.
 # This script installs the required modules on the host.
+#
+# script has to be executed as root!
+
+set -e
 
 # install requirements
-sudo apt-get install git libpcap-dev libnuma-dev pkg-config libtool gcc linux-headers-$(uname -r)
+apt-get install git libpcap-dev libnuma-dev pkg-config libtool gcc linux-headers-$(uname -r)
 
 # build PF_RING module
 cd PF_RING/kernel
-sudo make
-sudo make install
+make
+make install
 
 # load the module
-sudo insmod ./pf_ring.ko
+#insmod ./pf_ring.ko
 
+# install the module persistently
+
+cp pf_ring.ko /lib/modules/$(uname -r)/kernel/drivers/net/
+#chmod +x /lib/modules/$(uname -r)/kernel/drivers/net/pf_ring.ko
+echo "pf_ring" >> /etc/modules
 
