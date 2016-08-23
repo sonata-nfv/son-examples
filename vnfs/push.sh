b3f8bb5199b25dc8a13c7c82480c21cfacb21e2c
Steven Van Rossem <steven.vanrossem@intec.ugent.be>
2016-08-24 01:17:05 +0200

    push also the ryu vnf to the docker repo

--- vnfs/push.sh
+++ vnfs/push.sh
@@ -19,8 +19,11 @@ docker push $target_repo/sonata-snort-ids-vnf
 # ovs1 vnf
 docker push $target_repo/sonata-ovs1-vnf
 
-# ryu vnf
+# ryu-snort vnf
 docker push $target_repo/sonata-ryu-snort-vnf
+
+# ryu vnf
+docker push $target_repo/sonata-ryu-vnf
 
 # son-emu-sap vnf
 docker push $target_repo/son-emu-sap
@@ -28,6 +31,6 @@ docker push $target_repo/son-emu-sap
 # vtc vnf
 docker push $target_repo/sonata-vtc-vnf
 
-# snort IDS ovs vnf
+# snort IDS vnf for ovs fw
 docker push $target_repo/sonata-snort-ovs-vnf
 
