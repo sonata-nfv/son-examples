aa7cc44b4884d6149961499e4ab89c1adc26064f
Eduard Maas <edmaas@mail.upb.de>
2017-08-29 13:25:21 +0200

    fixed some issues with ips

--- vnfs/sonata-apache-bench-docker/ipconfig.sh
+++ vnfs/sonata-apache-bench-docker/ipconfig.sh
@@ -3,12 +3,12 @@
 sleep 1
 
 # IP setup (we need to try different names in different scenarios, but never eth0 which is the docker if)
-ifconfig "output" down
-ifconfig "output" 20.0.0.1 netmask 255.255.255.0
-ifconfig "output" up
+ifconfig "data" down
+ifconfig "data" 20.0.0.1 netmask 255.255.255.0
+ifconfig "data" up
 
 ifconfig "mgmt" down
-ifconfig "mgmt" 200.0.0.4 netmask 255.255.255.0
+ifconfig "mgmt" 200.0.0.1 netmask 255.255.255.0
 ifconfig "mgmt" up
 
 ifconfig > /ifconfig.debug
